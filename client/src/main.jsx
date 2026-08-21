805ee267bcb7da4100e099a6b922149c3e7b2176
Mohammad Arsal <mohammadjamshed1996@gmail.com>
2026-08-21 06:32:24 +0500

    feat: add React application entry

---
+++ client/src/main.jsx
@@ -0,0 +1,15 @@
+// Module: React application entry point.
+// This file mounts the root App component into the browser DOM.
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
